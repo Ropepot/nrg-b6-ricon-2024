5f40fd4071246a9e6ddde8356d4f9d28f9ce1872
Ropepot <156997930+Ropepot@users.noreply.github.com>
2024-10-08 08:10:29 +0000

    Added Primitives

---
+++ TaskSheet113/Primitives.java
@@ -0,0 +1,18 @@
+package TaskSheet113;
+
+public class Primitives {
+    public static void main(String[] args) {
+        
+        byte zero = 0;
+        char h = 'H';
+        int three = 3;
+        int one = 1;
+        float twoPointZero = 2.0f;
+        boolean True = true;
+
+        
+        String output = h + "" + three + one + one + zero + " w" + zero + "r" + one + "d " + twoPointZero + " " + True;
+
+        System.out.println(output); 
+    }
+}
